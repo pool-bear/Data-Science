37779c99c6bffd28c2519e63cbf37c09dea70810
pool-bear <32944992+pool-bear@users.noreply.github.com>
2021-11-15 15:54:31 -0500

    complete hw7

--- HW.07.Due.2021.11.16/hw7.py
+++ HW.07.Due.2021.11.16/hw7.py
@@ -1,17 +1,21 @@
+'''
+Tianyi Lu, UNI:tl3126, E-mail:tl3126@columbia.edu
+ACTU PS5841 Data Science Assignment 7
+'''
 from sklearn.linear_model import PoissonRegressor
 from statsmodels.api import Poisson
 import numpy as np
 import pandas as pd
 
+# Load the data
 df=pd.read_csv('~/Documents/data.csv')
 y=df['accidents'].values.reshape(-1,1)
 df['accidents_']=df['accidents']/df['personyears']
 y_=df['accidents_'].values.reshape(-1,1)
 x=df['age'].values.reshape(-1,1)
-reg_sk=PoissonRegressor(alpha=0).fit(x,np.ravel(y_))
-beta=np.array([[reg_sk.intercept_],[reg_sk.coef_[0]]])
-print(beta)
+x_=np.hstack((np.ones((len(y),1)),df['age'].values.reshape(-1,1)))
 
+# Log likelihood
 def log_likelihood(x,y,beta):
     lmb=np.exp(np.log(df['personyears'].values.reshape(-1,1))+beta[0][0]+beta[1][0]*x)
     output=0
@@ -21,17 +25,22 @@ def log_likelihood(x,y,beta):
             log_factorial+=np.log(j+1)
         output+=-lmb[i][0]+y[i][0]*np.log(lmb[i][0])-log_factorial
     return output
-print(log_likelihood(x,y,beta))
 
-x_=np.hstack((np.ones((len(y),1)),df['age'].values.reshape(-1,1)))
-reg_sm=Poisson(y,x_,offset=np.log(df['personyears'])).fit()
-beta=reg_sm.params.reshape(-1,1)
-print(reg_sm.summary())
-print(beta)
-l=log_likelihood(x,y,beta)
-print(l)
-beta_naive=[[np.log(np.average(y_))],[0]]
-l_naive=log_likelihood(x,y,beta_naive)
-r2=1-l/l_naive# todo: check this
-print(reg_sm.summary())
-print(r2)
+# Sklearn
+reg_sk=PoissonRegressor(alpha=0).fit(x,np.ravel(y_))
+beta_sk=np.array([[reg_sk.intercept_],[reg_sk.coef_[0]]])
+print("Sklearn results:")
+print("b0="+str(beta_sk[0][0])+"\nb1="+str(beta_sk[1][0]))
+print("Log likelihood="+str(log_likelihood(x,y,beta_sk))+"\n")
+
+# Statsmodels
+reg_sm=Poisson(y,x_,offset=np.log(df['personyears'])).fit(disp=0)
+beta_sm=reg_sm.params.reshape(-1,1)
+print("Statsmodels results:")
+print("b0="+str(beta_sm[0][0])+"\nb1="+str(beta_sm[1][0]))
+ll_sm=log_likelihood(x,y,beta_sm)
+print("Log likelihood="+str(ll_sm))
+reg_nv=Poisson(y,np.ones((len(y),1)),offset=np.log(df['personyears'])).fit(disp=0)
+beta_nv=np.vstack((reg_nv.params,np.zeros((1,1))))
+ll_nv=log_likelihood(x,y,beta_nv)
+print("Pseudo R2="+str(1-ll_sm/ll_nv))
